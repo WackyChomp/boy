7ee89994f7d737fa827f45e4eba5922954f839e6
Wilson Chan <wilsonchan54@gmail.com>
2024-01-12 21:43:28 -0500

    Added collection of colors to global.css / added colors to tailwind.config.ts / updated README.md

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent1: "#db4f4a",
+        accent2: '#3da58a',
+        biege: "#f9ffac",
+        gray: "#747474",
+      }
     },
   },
   plugins: [],
