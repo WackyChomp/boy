1daa78e955094e3b2ff79d4510f794b2f59d9d29
Wilson Chan <wilsonchan54@gmail.com>
2024-01-21 15:31:03 -0500

    Created util sortBlogs function to sort by descending publishedAt order / updated tsconfig.json for generating contentlayer / Fixed image filepath and added images to public directory to render blog image and meta data in HomeCoverSection component

--- components/Home/HomeCoverSection.tsx
+++ components/Home/HomeCoverSection.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
 import { allBlogs } from '@/.contentlayer/generated'
+import { sortBlogs } from '@/utils';
+import Link from 'next/link';
 
 type Props = {
   blogs: any;
 }
 
 const HomeCoverSection = ( {blogs}: Props) => {
+  const sortedBlogs = sortBlogs(blogs);
+  const blog = sortedBlogs[0];
+  
   return (
     <article className='bg-yellow-500 flex flex-col items-start justify-end mx-10 relative h-[85vh'>
       <Image 
-        src={blogs.image}
-        alt={blogs.title}
+        src={blog.image.filePath.replace('../public', '')}
+        alt={blog.title}
+        width='100'
         height='100'
         className='w-full h-full object-center object-cover rounded-3xl'
       />
+
+      <div className="w-3/4 space-y-5 p-16 flex flex-col items-start justify-center z-0">
+        <Link href={`/categories/${blog.tags[0]}`}>
+          Tags: {blog.tags.join(' || ')}
+        </Link>
+        <h1>Title: {blog.title}</h1>
+        <p>Description: {blog.description}</p>
+      </div>
     </article>
   )
 }
