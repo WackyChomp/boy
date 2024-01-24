ce437c55ed29f861babd0a7c9a10ac683f016128
Wilson Chan <wilsonchan54@gmail.com>
2024-01-24 18:18:21 -0500

    Created FeaturedPosts component for blog layout / created BlogLayoutOne component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { allBlogs } from '../.contentlayer/generated'
 import HomeCoverSection from '@/components/Home/HomeCoverSection'
+import FeaturedPosts from '@/components/Home/FeaturedPosts'
 
 export default function Home() {
   console.log(allBlogs)
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       Hello from the other side!!!
       <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
     </main>
   )
 }
---
+++ components/Blog/BlogLayoutOne.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Tag from '../Home/Elements/Tag';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type Props = {
+  blog: any;
+}
+
+const BlogLayoutOne = ({ blog }: Props) => {
+  return (
+    <div className="inline-block overflow-hidden rounded-xl">
+      
+      {/* dark overlay - same size as the overall div and blog image */}
+      <div 
+        className="absolute top-0 left-0 bottom-0 right-0 h-full
+        bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-10"
+      />
+    
+      <Image 
+        src={blog.image.filePath.replace('../public', '')}
+        alt={blog.title}
+        width={blog.image.width}
+        height={blog.image.height}
+        className='w-full h-full object-center object-cover rounded-3xl'
+      />
+
+      {/* Tag / Title / Description */}
+      <div className="w-full absolute bottom-0 p-10 z-20">
+        <Tag link={`/categories/${blog.tags[0]}`} name={blog.tags[0]}></Tag>
+
+        <Link href={blog.title} className='mt-6'>
+          <h1 className='font-bold captitalize text-4xl'>
+            <span className={`bg-gradient-to-r from-accent2 to-accent2 bg-[length:0px_6px] 
+              hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500
+            `}>
+              {blog.title}
+            </span>
+          </h1>
+        </Link>
+        
+        <p className='inline-block mt-4 text-xl font-r'>
+          Description: {blog.description}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default BlogLayoutOne
